06681f27119c9363a9c5fa6cb38ed8b4cfcd7733
Julien Viard de Galbert <julien.viarddegalbert@openwide.fr>
2015-02-04 23:00:41 +0100

    Adding rules for testing linux guest direclty on qemu
    Run the unpatched linux
    Allows testing the guest code without xvisor

--- make/kernel.mk
+++ make/kernel.mk
@@ -21,14 +21,26 @@ $(DISK_DIR)/$(DISK_BOARD)/$(KERN_IMG): $(LINUX_BUILD_DIR)/vmlinux \
   $(XVISOR_DIR)/$(XVISOR_ELF2C) $(XVISOR_BUILD_DIR)/$(XVISOR_CPATCH) \
   | $(DISK_DIR)/$(DISK_BOARD)
 	@echo "(patch) Linux"
+	$(Q)mv $< $<.bak
 	$(Q)$(XVISOR_DIR)/$(XVISOR_ELF2C) -f $< | \
 	  $(XVISOR_BUILD_DIR)/$(XVISOR_CPATCH) $< 0
 	$(Q)$(MAKE) -C $(LINUX_DIR) O=$(LINUX_BUILD_DIR) Image
-	$(Q)cp $(LINUX_BUILD_DIR)/arch/$(ARCH)/boot/Image $@
+	$(Q)mv $<.bak $<
+	$(Q)mv $(LINUX_BUILD_DIR)/arch/$(ARCH)/boot/Image $@
+
+$(LINUX_BUILD_DIR)/arch/$(ARCH)/boot/zImage: $(LINUX_BUILD_DIR)/vmlinux
+	$(Q)$(MAKE) -C $(LINUX_DIR) O=$(LINUX_BUILD_DIR) zImage
+
 
 $(LINUX_DIR)/arch/$(ARCH)/boot/dts/$(KERN_DT).dts: | $(LINUX_DIR)
 
 $(DISK_DIR)/$(DISK_BOARD)/$(KERN_DT).dtb: $(LINUX_DIR)/arch/$(ARCH)/boot/dts/$(KERN_DT).dts $(XVISOR_BUILD_DIR)/tools/dtc/dtc | $(XVISOR_DIR) $(DISK_DIR)/$(DISK_BOARD)
 	@echo "(dtc) $(KERN_DT)"
 	$(XVISOR_BUILD_DIR)/tools/dtc/dtc -I dts -O dtb -p 0x800 -o $@ $<
+
+linux-configure: $(LINUX_BUILD_CONF)
+
+linux-oldconfig linux-menuconfig linux-dtbs: | $(LINUX_BUILD_DIR) $(LINUX_DIR) TOOLCHAIN-prepare
+	@echo "($(subst linux-,,$@)) Linux"
+	$(Q)$(MAKE) -C $(LINUX_DIR) O=$(LINUX_BUILD_DIR) $(subst linux-,,$@)
 
--- make/qemu.mk
+++ make/qemu.mk
@@ -3,11 +3,28 @@ define QEMU
 	  -kernel $(BUILDDIR)/$(QEMU_IMG) -dtb $(BUILDDIR)/$(BOARDNAME).dtb
 endef
 
+QEMU_DISPLAY?=-display none
+QEMU_MONITOR?=-monitor telnet:127.0.0.1:1234,server,nowait
+
 qemu-run: $(BUILDDIR)/$(QEMU_IMG) $(BUILDDIR)/$(BOARDNAME).dtb
 	@echo "$@ for $(BOARDNAME)"
-	$(call QEMU,-display none -serial stdio,$<)
+	$(call QEMU,$(QEMU_DISPLAY) -serial stdio $(QEMU_MONITOR),$<)
 
 $(BUILDDIR)/$(QEMU_IMG): $(XVISOR_DIR)/$(MEMIMG) $(XVISOR_BIN) $(DISK_IMG)
 	@echo "(memimg) $(QEMU_IMG)"
 	$(Q)$< -a $(ADDR_HYPER) -o $@ $(XVISOR_BIN)@$(ADDR_HYPER) \
 		$(DISK_IMG)@$(ADDR_DISK)
+
+ifeq ($(USE_KERN_DT),1)
+  QEMU_KERN_DTB = -dtb $(DISKB_KERN_DTB)
+endif
+
+qemu-guest-run: $(LINUX_BUILD_DIR)/arch/$(ARCH)/boot/zImage $(DISKB_KERN_DTB) $(BUILDDIR)/$(ROOTFS_IMG)
+	/home/julienvdg/prog/build_qemu/arm-softmmu/qemu-system-$(ARCH) -M $(BOARDNAME) -m 256M $1 \
+	  -kernel $(LINUX_BUILD_DIR)/arch/$(ARCH)/boot/zImage \
+	  $(QEMU_KERN_DTB) \
+	  -initrd $(BUILDDIR)/$(ROOTFS_IMG) \
+	  -append "root=/dev/ram rw earlyprintk console=ttyAMA0" \
+	  -serial stdio \
+	  $(QEMU_DISPLAY) \
+	  $(QEMU_MONITOR)

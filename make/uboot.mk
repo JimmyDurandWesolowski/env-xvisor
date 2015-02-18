f1edb1febbc4c180add7159a8ddd2a852d208a53
Julien Viard de Galbert <julien.viarddegalbert@openwide.fr>
2015-02-18 23:30:18 +0100

    update uboot options

--- make/uboot.mk
+++ make/uboot.mk
@@ -1,4 +1,4 @@
-ifneq ($(BOARD_UBOOT),)
+ifeq ($(BOARD_UBOOT),1)
   $(UBOOT_DIR)/$(UBOOT_BOARD_CFG): $(UBOOT_DIR)
 
   # At this time, there is no other way to generate the .cfgtmp than building
@@ -6,10 +6,10 @@ ifneq ($(BOARD_UBOOT),)
   $(UBOOT_BUILD_DIR)/$(UBOOT_BOARD_CFG).cfgtmp: \
     $(UBOOT_DIR)/$(UBOOT_BOARD_CFG) | $(TOOLCHAIN_DIR)
 	@echo "(defconfig) U-Boot"
-	$(Q)$(MAKE) -C $(UBOOT_DIR) O=$(UBOOT_BUILD_DIR) $(UBOOT_BOARDNAME)_config
+	$(Q)$(MAKE) -C $(UBOOT_DIR) O=$(UBOOT_BUILD_DIR) $(UBOOT_BOARDNAME)
 
   $(UBOOT_BUILD_DIR)/include/config.h: $(UBOOT_DIR) | $(TOOLCHAIN_DIR)
-	$(Q)$(MAKE) -C $(UBOOT_DIR) O=$(UBOOT_BUILD_DIR) $(UBOOT_BOARDNAME)_config
+	$(Q)$(MAKE) -C $(UBOOT_DIR) O=$(UBOOT_BUILD_DIR) $(UBOOT_BOARDNAME)
 
   uboot-configure: $(UBOOT_BUILD_DIR)/include/config.h
 

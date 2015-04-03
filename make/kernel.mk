a791cbf70a0021aa215be21dbd866cd902807d94
Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
2015-04-03 15:21:31 +0100

    Add license on all files for public release.

--- make/kernel.mk
+++ make/kernel.mk
@@ -1,3 +1,26 @@
+#
+# This file is part of Xvisor Build Environment.
+# Copyright (C) 2015 Institut de Recherche Technologique SystemX
+# Copyright (C) 2015 OpenWide
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this Xvisor Build Environment. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# @file make/kernel.mk
+#
+
 BOARDNAME_CONF?=$(shell echo $(GUEST_BOARDNAME) | tr '-' '_')
 XVISOR_LINUX_CONF_DIR=$(XVISOR_DIR)/tests/$(XVISOR_ARCH)/$(GUEST_BOARDNAME)/linux
 XVISOR_LINUX_CONF_NAME=$(LINUX_PATH)_$(BOARDNAME_CONF)_defconfig
--- make/openocd.mk
+++ make/openocd.mk
@@ -1,3 +1,26 @@
+#
+# This file is part of Xvisor Build Environment.
+# Copyright (C) 2015 Institut de Recherche Technologique SystemX
+# Copyright (C) 2015 OpenWide
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this Xvisor Build Environment. If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# @file make/openocd.mk
+#
+
 # OpenOCD configure is buggy, reconfigure it
 $(STAMPDIR)/.openocd_reconf: $(OPENOCD_DIR) | $(STAMPDIR)
 	$(Q)cd $< && autoreconf --force --install

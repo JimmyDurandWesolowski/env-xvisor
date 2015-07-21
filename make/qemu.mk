236fc9973768bb987d14fdfbdeb36f7bdc34d7d8
Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
2015-07-21 15:29:32 +0200

    Update the environment for Xvisor initrd support.

--- make/qemu.mk
+++ make/qemu.mk
@@ -23,27 +23,25 @@
 
 ifeq ($(BOARD_QEMU),1)
 define QEMU
-	qemu-system-$(ARCH) -M $(BOARDNAME) -m 256M $1 \
-	  -kernel $(BUILDDIR)/$(QEMU_IMG) -dtb $(BUILDDIR)/$(BOARDNAME).dtb
+	qemu-system-$(ARCH) -M $(BOARDNAME) -m 256M $1		\
+	  -kernel $(XVISOR_BIN)					\
+	  -dtb $(BUILDDIR)/$(BOARDNAME).dtb			\
+	  -initrd $(BUILDDIR)/$(ROOTFS_IMG)
 endef
 
 QEMU_DISPLAY?=-display none
 QEMU_MONITOR?=-monitor telnet:127.0.0.1:1234,server,nowait
 QEMU_EXTRA?=
 
-qemu-run: $(BUILDDIR)/$(QEMU_IMG) $(BUILDDIR)/$(BOARDNAME).dtb
+qemu-run: $(XVISOR_BIN) $(BUILDDIR)/$(BOARDNAME).dtb $(BUILDDIR)/$(ROOTFS_IMG)
 	@echo "$@ for $(BOARDNAME)"
 	$(call QEMU,$(QEMU_DISPLAY) -serial stdio $(QEMU_MONITOR) $(QEMU_EXTRA),$<)
-
-$(BUILDDIR)/$(QEMU_IMG): $(XVISOR_DIR)/$(MEMIMG) $(XVISOR_BIN) $(DISK_IMG)
-	@echo "(memimg) $(QEMU_IMG)"
-	$(Q)$< -a $(ADDR_HYPER) -o $@ $(XVISOR_BIN)@$(ADDR_HYPER) \
-		$(DISK_IMG)@$(ADDR_DISK)
 
 ifeq ($(USE_KERN_DT),1)
   QEMU_KERN_DTB = -dtb $(DISKB_KERN_DTB)
 endif
 
+# Run the Linux guest without Xvisor for tests
 qemu-guest-run: $(LINUX_BUILD_DIR)/arch/$(ARCH)/boot/zImage $(DISKB_KERN_DTB) $(BUILDDIR)/$(ROOTFS_IMG)
 	qemu-system-$(ARCH) -M $(BOARDNAME) -m 256M $1 \
 	  -kernel $(LINUX_BUILD_DIR)/arch/$(ARCH)/boot/zImage \
